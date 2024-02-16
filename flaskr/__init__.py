e0d58943b2f021fbbe9ae90b097fa88957a963c4
Shehab Magdy El-Adl <cegres1@hotmail.com>
2024-02-16 22:05:17 +0200

    generate auth blueprint

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask
-from . import db
+from . import db, auth
 
 def create_app(test_config=None):
     # Create and configure the app
@@ -29,5 +29,6 @@ def create_app(test_config=None):
         return('Hello World!')
     
     db.init_app(app)
-    
+    app.register_blueprint(auth.pb)
+
     return app
